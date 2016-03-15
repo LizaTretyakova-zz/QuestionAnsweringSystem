f57427cf706d315183f7b1ff10f695b321c334e8
N-buga <bugakova_nadya@mail.ru>
2016-03-16 01:12:20 +0300

    restruction everything, get classes

--- src/prototype.py
+++ src/prototype.py
@@ -1,149 +1,210 @@
 from sys import *
 import re
+from enum import Enum
 
-import itertools
+
+class Question_Subject(Enum):
+    download = 0
+    event = 1
+    money = 2
+
+
+class Answer_Type(Enum):
+    person = 0
+    description = 1
+    count = 2
+    explanation = 3
+    place = 4
+    time = 5
+
+
+class Query:
+    def __init__(self, words):
+        self.answer_type = None
+        self.words = words
+        self.possible_dates = set()
+        self.possible_countries = set()
+        self.answer_type = None
+
+    def set_answer_type(self, answer_type):
+        self.answer_type = answer_type
+
+    def find_time(self):
+        for word in words:
+            if word.isdigit():
+                self.possible_dates.add(word)
+        if len(self.possible_dates) == 0: self.possible_dates.add(None)
+
+    def find_country(self):
+        previous_word = None
+        for word in words:
+            if (previous_word == "in") and (ask_country_base(word) != 0):
+                self.possible_countries.add(word)
+            elif (word in download_words) and (ask_nation_base(previous_word) != 0):
+                self.possible_countries.add(ask_nation_base(previous_word, "country"))
+            previous_word = word
+        if len(self.possible_countries) == 0: self.possible_countries.add(None)
+
+
+class Query_event(Query):
+    def __init__(self, words):
+        Query.__init__(self, words)
+        self.action = None
+        self.participant = None
+
+    def find_action(self):
+        if self.words[1] == "is":
+            if len(self.words) > 4 and self.words[3][-3:-1] == "ing":
+                self.action = words[3]
+        elif len(self.words) > 3:
+            if not self.words[3].isdigit(): self.action = self.words[3]
+
+    def find_participant(self):
+        self.participant = self.words[2]
+
+    def get_answer(self):
+        self.find_country()
+        self.find_time()
+        self.find_participant()
+        self.find_action()
+        for country in self.possible_countries:
+            for date in self.possible_dates:
+                ask_events_database(self.answer_type, country, date, self.participant, self.action)
+
+
+class Query_money(Query):
+    def __init__(self, words):
+        Query.__init__(self, words)
+        self.possible_products = set()
+
+    def find_product(self):
+        for word in words:
+            if ask_product_base(word) != 0:
+                self.possible_products.add(word)
+        if len(self.possible_products) == 0: self.possible_products.add(None)
+
+    def get_answer(self):
+        self.find_country()
+        self.find_time()
+        self.find_product()
+        for country in self.possible_countries:
+            for date in self.possible_dates:
+                for product in self.possible_products:
+                    return ask_money_database(self.answer_type, country, date, product)
+
+class Query_downloads(Query):
+    def __init__(self, words):
+        Query.__init__(self, words)
+        self.possible_products = set()
+
+    def find_product(self):
+        for word in words:
+            if ask_product_base(word) != 0:
+                self.possible_products.add(word)
+        if len(self.possible_products) == 0: self.possible_products.add(None)
+
+    def get_answer(self):
+        self.find_country()
+        self.find_time()
+        self.find_product()
+        for country in self.possible_countries:
+            for date in self.possible_dates:
+                for product in self.possible_products:
+                    ask_download_database(self.answer_type, country, date, product)
 
 contries = set(["China"])
 products = set(["PyCharm"])
 download_words = {"downloads", "download", "downloaded"}
 
-def ask_events_database(ask_word, participent, action, date):
-        print("events, ", ask_word, participent, action, date)
-        print()
-        
-def ask_download_database(ask_word, product, contry, date):
-        print("downloads, ", ask_word, product, contry, date) 
-        print()
 
-def ask_money_database(ask_word, product, base, country):
-        print("money, ", ask_word, product, base, country)
-        print()         
-        
-def ask_nation_base(word, retrun_group = None):
-        if (retrun_group != None): return "a"
-        else: return False
-def ask_country_base(word, retrun_group = None):
-        if (retrun_group != None): return "a"
+def ask_events_database(ask_word, country, date, participant, action):
+    print("events, ", ask_word, participant, action, date)
+    print()
+
+
+def ask_download_database(ask_word, country, date, product):
+    print("downloads, ", ask_word, product, country, date)
+    print()
+
+
+def ask_money_database(ask_word, country, date, product):
+    print("money, ", ask_word, product, date, country)
+    print()
+
+
+def ask_nation_base(word, retrun_group=None):
+    if retrun_group is not None:
+        return "a"
+    else:
+        return False
+
+
+def ask_country_base(word, retrun_group=None):
+    if retrun_group is not None:
+        return "a"
+    else:
+        if word in contries:
+            return True
         else:
-                if (word in contries):
-                        return True 
-                else: 
-                        return False
-def ask_product_base(word, retrun_group = None):
-        if (word in products):
-                return True
-        else:
-                return False        
-        
-def events_handler(words):
-        ask_word = "date"
-        participent = words[2]
-        possible_datas = set()
-        action = None
-        for word in words:
-                if (word.isdigit()):
-                        possible_datas.add(word)
-        if (words[1] == "is"):
-                if (len(words) > 4 and words[3][-3:-1] == "ing"):
-                        action = words[3]
-        elif (len(words) > 3):
-                if (not word.isdigit()): action = words[3]
-        if (len(possible_datas) == 0): possible_datas.add(None)                
-        for data in possible_datas:
-                ask_events_database(ask_word, participent, action, data)
-                        
-                                                                        
-def download_handler(words):
-        ask_word = "how many downloads"
-        possible_products = set()
-        possible_countries = set()
-        possible_dates = set()
-        for word in words:
-                if (ask_product_base(word) != 0):
-                        possible_products.add(word)
-        if (len(possible_products) == 0): possible_products.add(None)  
-        
-        previous_word = None                      
-        for word in words:          
-                if (previous_word == "in") and (ask_country_base(word) != 0):
-                        possible_countries.add(word)
-                elif (word in download_words) and (ask_nation_base(previous_word) != 0):
-                        possible_countries.add(ask_nation_base(previous_word,"country"));                                                      
-                previous_word = word
-        if (len(possible_countries) == 0): possible_countries.add(None)
-        for word in words:
-                if (word.isdigit()):
-                        possible_dates.add(word)
-        if (len(possible_dates) == 0): possible_dates.add(None)                        
-        variants = itertools.product(possible_products, possible_countries, possible_dates)
-        for variant in variants:
-                ask_download_database(ask_word, variant[0], variant[1], variant[2])                               
-                        
-        
-def money_handler(words):
-        possible_products = set()
-        possible_dates = set()
-        possible_countries = set()                               
+            return False
 
-        for word in words:
-                if (ask_product_base(word) != 0):
-                        possible_products.add(word)
-        if (len(possible_products) == 0): possible_products.add(None)
 
-        for word in words:
-                if (word.isdigit()):
-                        possible_dates.add(word)
-        if (len(possible_dates) == 0): possible_dates.add(word)                        
+def ask_product_base(word, retrun_group=None):
+    if word in products:
+        return True
+    else:
+        return False
 
-        previous_word = None
-        for word in words:
-                if (previous_word == "in" and ask_country_base(word)):
-                        possible_countries.add(word)
-                if (ask_nation_base(previous_word) and word == "customers"):
-                        possible_countries.add(ask_nation_base(previous_word, "country"))
-                previous_word = word
-        if (len(possible_countries) == 0): possible_countries.add(None)
-
-        variants = itertools.product(possible_products, possible_dates, possible_countries);
-        if ("customers" in words):
-                ask_word = "count of customers"
-        else:
-                ask_word = "count of sold products"
-        for variant in variants:
-                ask_money_database(ask_word, variant[0], variant[1], variant[2]);
-                                                
 
 def who_function(words):
-        print("who, Incorrect question, please try again.")
-        print()
+    return Answer_Type.person
+
+
 def what_function(words):
-        print("what, Incorrect question, please try again.")
-        print()
+    return Answer_Type.description
+
+
 def how_function(words):
-        if (words[1] != "many"):
-                print("Incorrect question, please try again.")
-                print()
-                return
-        if (len(set(words) & download_words) == 0):
-                money_handler(words)
-        else: download_handler(words)
-        
+    if words[1] == "many" or words[1] == "much":
+        return Answer_Type.count
+    return Answer_Type.explanation
+
+
 def where_function(words):
-        print("Incorrect question, please try again.")
+    return Answer_Type.place
+
+
 def when_function(words):
-        events_handler(words)
+    return Answer_Type.time
+
+
+ask_word_function_mapping = {
+    "Who": who_function,
+    "What": what_function,
+    "How": how_function,
+    "Where": where_function,
+    "When": when_function,
+}
+
+
+def detect_question_subject(words):
+    for word in words:
+        if word in download_words:
+            return Query_downloads(words)
+    if words[0] == "How" and (words[1] == "much" or words[1] == "many"):
+        return Query_money(words)
+    return Query_event(words)
+
+
+def detect_answer_type(words):
+    return ask_word_function_mapping[words[0]](words)
+
 
 if __name__ == "__main__":
-	words = input().split()
-	words[-1] = re.match('(\w)*', words[-1]).string
-	print(words)
-
-	word_function_mapping = {
-        	"Who": who_function,
-        	"What": what_function,
-        	"How": how_function,
-        	"Where": where_function,
-        	"When": when_function,        
-	}
-
-	word_function_mapping[words[0]](words)
+    words = input().split()
+    words[-1] = re.match('(\w)*', words[-1]).group(0)
+    print(words)
+    question_subject = detect_question_subject(words)
+    answer_type = detect_answer_type(words)
+    question_subject.set_answer_type(answer_type)
+    print(question_subject.get_answer())
